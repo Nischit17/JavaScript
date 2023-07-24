ed7a4418ffa6d42471ce3d7ceba9588ba8fa9e6a
unknown <nischit819@gmail.com>
2023-07-24 17:20:49 +0530

    JS

---
+++ Assignments/Fundamentals Part - 1/Values and Variables/script.js
@@ -0,0 +1,9 @@
+// Declare variables called 'country', 'continent' and 'population' and assign their values accordingly to our own country (population in millions) and log their values to the console.
+
+let country = 'India';
+let continent = 'Asia';
+let population = 1393.4;
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ Fundamentals-PART1/script.js
@@ -0,0 +1,51 @@
+// let js = 'amazing';
+// console.log(40+8+23-10);
+
+// console.log('Nischit');
+// console.log(23);
+
+// let firstName = "NISCHIT";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+
+// DATA TYPES
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Nischit');
+
+// javascriptIsFun = 'YES!';
+// console.log(javascriptIsFun);
+
+// let year; 
+// console.log(year);
+// console.log(typeof year);
+
+// year = 2023;
+// console.log(year);
+
+// console.log(typeof null);
+
+
+// let, const and var
+// let age = 30;
+// age = 31;
+
+// const birthYear = 2001;
+// // birthYear = 1990;
+
+// var job = 'programmer';
+// job = 'teacher';
+
+// lastName = 'D S';
+// console.log(lastName);
+
+
+
+
