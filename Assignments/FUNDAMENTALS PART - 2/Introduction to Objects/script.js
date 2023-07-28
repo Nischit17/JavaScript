71d336de7e029bb9ca70de9059dcee780344c6f1
unknown <nischit819@gmail.com>
2023-07-28 13:49:59 +0530

    JS

---
+++ Assignments/FUNDAMENTALS PART - 2/Introduction to Objects/script.js
@@ -0,0 +1,9 @@
+// 1) Create an object called 'myCountry' for a country of your choice, containing properties 'country', 'capital', 'language', 'population' and 'neighbours' (an array like we used in previous assignments)
+
+const myCountry = {
+  country: 'Finland',
+  capital: 'Helsinki',
+  language: 'finnish',
+  population: '6',
+  neighbours: ['Norway', 'Sweden', 'Russia']
+};
--- Class/Fundamentals-PART2/script.js
+++ Class/Fundamentals-PART2/script.js
@@ -119,4 +119,143 @@ const yearsUntilRetirement = function(birthYear, firstName) {
 }
 console.log(yearsUntilRetirement(1991, 'Nischit'));
 console.log(yearsUntilRetirement(1950, 'Mike'));
+
+
+
+// ARRAYS //
+
+'use strict';
+
+const friend1 = 'Likith';
+const friend2 = 'Sharad';
+const friend3 = 'Gagan';
+
+// METHOD - 1
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+// METHOD - 2
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length-1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice']
+
+const firstName = 'Jonas'
+const Jonas = [firstName, 'Schmedtmann', 2037 - 1991, 'teacher', friends];
+console.log(Jonas);
+console.log(Jonas.length);
+
+// Exercise
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+const years = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge (years[0]);
+const age2 = calcAge (years[1]);
+const age3 = calcAge (years[years.length-1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge (years[0]), calcAge (years[1]), calcAge (years[years.length-1])];
+console.log(ages);
+
+
+
+
+// BASIC ARRAY OPERATION (METHODS) //
+
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+
+
+friends.unshift('John'); // Adds element 1st
+console.log(friends);
+
+const newLength = friends.push('Jay'); // Adds element Last
+console.log(friends);
+console.log(newLength);
+
+
+// Remove elements
+
+friends.shift(); // Removes Frist element
+console.log(friends);
+
+friends.pop(); // Removes Last element
+const popped = friends.pop(); 
+console.log(popped);
+console.log(friends);
+
+console.log(friends.indexOf('Steven')); // Index element
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('Steven')); // T OR F
+console.log(friends.includes('Bob'));
+console.log(friends.includes(23));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Steven');
+}
+
+
+// OBJECTS //
+
+// Object literal contents {}
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+
+
+
+// Dot vs. Bracket Notation //
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const intrestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends');
+
+if(jonas[intrestedIn]) {
+  console.log(jonas[intrestedIn]);
+} else {
+  console.log('Wrong request! Choose between firstName, lastName, age, job, and friends');
+}
+
+jonas.location = 'Portugal';
+jonas['Twitter'] = '@xyz123';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
 */
+
+
+
