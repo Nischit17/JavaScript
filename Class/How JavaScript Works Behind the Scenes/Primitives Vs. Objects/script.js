20f7c8df56096d0fa8055015313233e89d53fe8d
unknown <nischit819@gmail.com>
2023-08-04 18:06:32 +0530

    JS

--- Class/How JavaScript Works Behind the Scenes/Primitives Vs. Objects/script.js
+++ Class/How JavaScript Works Behind the Scenes/Primitives Vs. Objects/script.js
@@ -1,1 +1,53 @@
 'use strict';
+/*
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+const friend = me;
+friend.age = 27;
+console.log('Friend:', friend);
+console.log('Me', me);
+*/
+
+// Primitive types
+lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// Reference types
+const jessica = {
+  firsName: 'jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+// marriedJessica = {};  This don't work.
+
+// Copying objects
+const jessica2 = {
+  firsName: 'jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
